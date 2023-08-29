5e959f92bc73f75eb26a84d1269c5c3620a3f0d0
Yassine Lafraouzi <mhtajek.sup@gmail.com>
2023-08-29 14:40:44 -0700

    adv 0x06

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,67 +1,110 @@
 #!/usr/bin/python3
+"""Defines the classes Node and SinglyLinkedList"""
+
+
 class Node:
+    """
+    Class that defines properties Node.
+
+    Attributes:
+        data: data field of node.
+    """
     def __init__(self, data, next_node=None):
-        """Defines a node of a singly linked list"""
+        """Creates new instances of node.
+
+        Args:
+            __data : data field of node.
+        """
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
-        """data getter"""
+        """Retrieves the data field instance.
+
+        Returns: the data field of a node.
+        """
         return self.__data
 
     @data.setter
     def data(self, value):
-        """data setter"""
-        if type(value) != int:
+        """Propery setter for data.
+
+        Args:
+            value (int): data field of a node.
+
+        Raises:
+            TypeError: data must be an integer
+        """
+        if not isinstance(value, int):
             raise TypeError("data must be an integer")
         self.__data = value
 
     @property
     def next_node(self):
-        """next_node getter"""
+        """Retrives the next_node instance.
+
+        Returns: The next_node instance.
+        """
         return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
-        """next_node setter"""
-        if value is not None and type(value) is not Node:
+        """Property setter for Node.
+
+        Args:
+            value (None): next node of a Node.
+
+        Raises:
+            TypeError: next_node must be a Node object .
+        """
+        if value is not None and not isinstance(value, Node):
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
 
 class SinglyLinkedList:
+    """
+    Class that defines properties of SinglyLinkedList.
+
+    Attributes:
+        head: head of the SinglyLinkedList.
+    """
     def __init__(self):
-        """Defines a singly linked list"""
+        """Creates new instances of SinglyLinkedList .
+
+        Args:
+            __head : head of the SinglyLinkedList .
+        """
         self.__head = None
 
-    def sorted_insert(self, value):
-        new = Node(value)
-        tmp = self.__head
-        add_start = False
-
-        if not self.__head:
-            self.__head = new
-            new.next_node = None
-        else:
-            if value < self.__head.data:
-                add_start = True
-            while tmp.next_node and value > tmp.next_node.data\
-                    and not add_start:
-                tmp = tmp.next_node
-            if not add_start:
-                    new.next_node = tmp.next_node
-                    tmp.next_node = new
-            else:
-                new.next_node = tmp
-                self.__head = new
-            new.data = value
-
     def __str__(self):
-        s = ""
-        current = self.__head
+        """Represents the class objects as a string.
 
-        while current:
-            s += str(current.data) + '\n'
-            current = current.next_node
-        return s[: -1]
+        Returns: The class object represented as a string.
+        """
+        temp_var = self.__head
+        print_node = []
+        while temp_var:
+            print_node.sort()
+            print_node.append(str(temp_var.data))
+            temp_var = temp_var.next_node
+
+        print_node.sort(key=int)
+        return ("\n".join(print_node))
+
+    def sorted_insert(self, value):
+        """Inserts a new node at a given position.
+
+        Args:
+            value: value.
+        """
+        if self.__head is None:
+            new_node = Node(value)
+            new_node.next_node = self.__head
+            self.__head = new_node
+        else:
+            new_node = Node(value)
+            new_node.data = value
+            new_node.next_node = self.__head
+            self.__head = new_node
--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,42 +1,115 @@
 #!/usr/bin/python3
+"""Defines a class Square"""
+
+
 class Square:
-    """Defines a square"""
+    """
+    Class that defines properties of square by: (based on 3-square.py).
+
+    Attributes:
+        size: size of a square (1 side).
+    """
     def __init__(self, size=0):
-        """Initialises the data"""
-        self.size = size
+        """Creates new instances of square.
+
+        Args:
+            size: size of the square (1 side).
+        """
+        self.__size = size
 
     def area(self):
-        """Returns current square area"""
-        return self.__size**2
+        """Calculates the area of square.
+
+        Returns: the current square area.
+        """
+        return self.__size ** 2
 
     @property
     def size(self):
-        """Getter method"""
+        """Returns the size of a square
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Setter method"""
-        self.__size = value
-        if type(value) != int:
+        """Property setter for size.
+
+        Args:
+            value (int): size of a square (1 side).
+
+        Raises:
+            TypeError: size must be an integer
+            ValueError: size must be >= 0
+        """
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
 
     def __lt__(self, other):
-        return self.area() < other.area()
+        """Rich comparison operator to compare if square area is less
+        than another.
+
+        Args:
+            other (Square): square to compare size to.
+
+        Returns: True or false.
+        """
+        return self.__size < other.__size
 
     def __le__(self, other):
-        return self.area() <= other.area()
+        """Rich comparison operator to compare if square area is less
+        than or equal to another.
+
+        Args:
+            other (Square): square to compare size to.
+
+        Returns: True or false.
+        """
+        return self.__size <= other.__size
 
     def __eq__(self, other):
-        return self.area() == other.area()
+        """Rich comparison operator to compare if square area is equal to
+        another.
+
+        Args:
+            other (Square): square to compare size to.
+
+        Returns: True or false
+        """
+        return self.__size == other.__size
 
     def __ne__(self, other):
-        return self.area() != other.area()
+        """Rich comparison operator to compare if square area is not
+        equal to another.
+
+        Args:
+            other (Square): square to compare size to.
+
+        Returns: True or false
+        """
+        return self.__size != other.__size
 
     def __gt__(self, other):
-        return self.area() > other.area()
+        """Rich comparison operator to compare if square area is greater
+        than another.
+
+        Args:
+            other (Square): square to compare size to.
+
+        Returns: True or false
+        """
+        return self.__size > other.__size
 
     def __ge__(self, other):
-        return self.area() >= other.area()
+        """Rich comparison operator to compare if square area is greater
+        than or equal to another.
+
+        Args:
+            other (Square): square to compare size to.
+
+        Returns: True or false
+        """
+        return self.__size >= other.__size
